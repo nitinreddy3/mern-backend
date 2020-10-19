58fd55e08050ab69c48d3f5567d330ba235e0906
nitin reddy <reddynitin3@gmail.com>
2020-10-19 20:21:03 +0530

    Done

--- server/server.js
+++ server/server.js
@@ -1,33 +1,21 @@
-import express from 'express'
-import devBundle from './devBundle'
-import path from 'path'
-import template from './../template'
-import { MongoClient } from 'mongodb'
+import config from './../config/config'
+import mongoose from 'mongoose'
+import app from './express'
 
-const CURRENT_WORKING_DIR = process.cwd()
-const app = express()
 
-devBundle.compile(app)
-
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR,
-  'dist')))
-
-app.get('/', (req, res) => {
-  res.status(200).send(template())
-})
-
-let port = process.env.PORT || 3000
-app.listen(port, function onStart(err) {
+app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
   }
-  console.info('Server started on port %s.', port)
+  console.info('Server started on port %s.', config.port)
 })
 
-// DB configurations
-const url = process.env.MONGODB_URI ||
-  'mongodb://localhost:27017/mernSimpleSetup'
-MongoClient.connect(url, (err, db) => {
-  console.log("Connected successfully to mongodb server")
-  db.close()
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`)
 })
